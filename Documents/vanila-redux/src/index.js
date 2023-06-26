88c7691c1c30836aba0c7082247c3fb1864f6c22
goeunP <0young384@gmail.com>
2023-06-26 13:28:15 +0900

    vanila redux-1

--- Documents/vanila-redux/src/index.js
+++ Documents/vanila-redux/src/index.js
@@ -14,3 +14,27 @@
 // // to log results (for example: reportWebVitals(console.log))
 // // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const num = document.querySelector("span");
+
+let count = 0;
+
+const updateText = () => {
+  num.innerText = count;
+};
+
+const handleAdd = () => {
+  count += 1;
+  updateText();
+};
+
+const handleMinus = () => {
+  count -= 1;
+  updateText();
+};
+
+add.addEventListener("click", handleAdd);
+
+minus.addEventListener("click", handleMinus);
