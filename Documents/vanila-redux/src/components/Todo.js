6503bab471709e1b740108f0e8631706fbdc3749
goeunP <0young384@gmail.com>
2023-06-27 16:46:30 +0900

    save data with localStorage

--- Documents/vanila-redux/src/components/Todo.js
+++ Documents/vanila-redux/src/components/Todo.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useDispatch } from "react-redux";
 import { deleteTodo } from "../store";
 import { Link } from "react-router-dom";
 import styled from "@emotion/styled";
--- Documents/vanila-redux/src/store.js
+++ Documents/vanila-redux/src/store.js
@@ -11,12 +11,20 @@ export const deleteTodo = (id) => {
   return { type: DELETE, id };
 };
 
-const reducer = (state = [], action) => {
+// JSON.stringify => js 값이나 객체를 JSON 문자열로 변환
+// JSON.parse => JSON 문자열은 js 객체로 변환
+//localStorage.setItem("todos", JSON.stringify([]));
+
+const reducer = (state = JSON.parse(localStorage.getItem("todos")), action) => {
   switch (action.type) {
     case ADD:
-      return [{ text: action.text, id: Date.now() }, ...state];
+      const newToDo = { text: action.text, id: Date.now() };
+      localStorage.setItem("todos", JSON.stringify([newToDo, ...state]));
+      return JSON.parse(localStorage.getItem("todos"));
     case DELETE:
-      return state.filter((toDo) => toDo.id !== action.id);
+      const delToDo = state.filter((toDo) => toDo.id !== action.id);
+      localStorage.setItem("todos", JSON.stringify(delToDo));
+      return JSON.parse(localStorage.getItem("todos"));
     default:
       return state;
   }
